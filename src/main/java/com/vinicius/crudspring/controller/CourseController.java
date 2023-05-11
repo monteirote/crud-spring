7c8620911dfdc3ef04b6930344e8f1d93931392a
Vinícius Monteiro <vinicius@iconax.com.br>
2023-05-11 16:25:31 -0300

    aula 16

--- src/main/java/com/vinicius/crudspring/controller/CourseController.java
+++ src/main/java/com/vinicius/crudspring/controller/CourseController.java
@@ -2,10 +2,9 @@ package com.vinicius.crudspring.controller;
 
 import java.util.List;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.crudspring.models.Course;
@@ -21,7 +20,7 @@ public class CourseController {
     private CourseRepository CourseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return CourseRepository.findAll();
     }
 
