31cb1d5a2821935daf37a8724b0c19afc5d5bcc6
Vinícius Monteiro <vinicius@iconax.com.br>
2023-05-19 11:42:26 -0300

    aula 21

--- src/main/java/com/vinicius/crudspring/controller/CourseController.java
+++ src/main/java/com/vinicius/crudspring/controller/CourseController.java
@@ -2,9 +2,14 @@ package com.vinicius.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.crudspring.models.Course;
@@ -17,11 +22,17 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-    private CourseRepository CourseRepository;
+    private CourseRepository courseRepository;
 
     @GetMapping
     public @ResponseBody List<Course> list() {
-        return CourseRepository.findAll();
+        return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 
 }
