389dd2a4c7d5c76b88908f359f15945d4e50a22d
Vinícius Monteiro <vinicius@iconax.com.br>
2023-05-24 18:10:01 -0300

    Update CourseController.java

--- src/main/java/com/vinicius/crudspring/controller/CourseController.java
+++ src/main/java/com/vinicius/crudspring/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.vinicius.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,13 @@ public class CourseController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
     }
 
 }
